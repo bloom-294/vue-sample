066c9fe767b711a3ded205892c6dd8e30d6e9203
bloom <non44921@gmail.com>
2024-04-02 10:33:12 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue';
+import Payment from './components/Payment.vue';
+</script>
+
+<template>
+  <section class="container">
+    <Payment />
+  </section>
+</template>
+
+<style scoped>
+.container {
+  /* background-color: white; */
+  height: 90vh;
+  width: 90vw;
+}
+</style>
