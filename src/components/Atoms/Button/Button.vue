f33ad2356876337c1d0e8f8a08be3d900f0e9cac
bloom <non44921@gmail.com>
2024-04-22 16:36:57 +0900

    Buttonコンポーネント作成

---
+++ src/components/Atoms/Button/Button.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  text?: string,
+  type?: "primary" | "secondary" | "other",
+  size?: "small" | "medium" | "large"
+}>();
+
+const types = computed(() => {
+ switch(props.type){
+  case "primary":
+    return "is-primary";
+  case "secondary":
+    return "is-secondary";
+  case "other":
+    return "is-other";
+  default: break;
+}
+});
+
+const sizes = computed(() => {
+ switch(props.size){
+  case "small":
+    return "is-small";
+  case "medium":
+    return "is-medium";
+  case "large":
+    return "is-large";
+  default: break;
+}
+});
+
+</script>
+
+<template>
+  <button type="submit" class="button" :class="types, sizes">{{ props.text || "Button" }}</button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  width: 120px;
+  height: auto;
+  border-radius: 12px;
+  background-color: #F3B8AF;
+  color: #fff;
+  border: unset;
+  aspect-ratio: 15/4;
+  text-align: center;
+  font-size: 20px;
+  cursor: pointer;
+  padding: 10px;
+  z-index: 10;
+
+  &:hover {
+    background-color: #98B9D2;
+    transition: all .5s ease-out;
+  }
+
+  &.is {
+    &-small {
+      width: 80px;
+      border-radius: 8px;
+      font-size: 16px;
+    }
+    
+    &-medium {
+      width: 140px;
+      border-radius: 14px;
+      font-size: 24px;
+    }
+    
+    &-large {
+      width: 200px;
+      border-radius: 20px;
+      font-size: 28px;
+    }
+
+    &-primary {
+      background-color: #BDC597;
+      
+      &:hover {
+        background-color: #98B9D2;
+      }
+    }
+    
+    &-secondary {
+      background-color: #98B9D2;
+      
+      &:hover {
+        background-color: #E9C872;
+      }
+    }
+    
+    &-other {
+      background-color: #E9C872;
+      aspect-ratio: 1/1;
+      border-radius: 100%;
+      
+      &:hover {
+        background-color: #F3B8AF;
+      }
+    }    
+
+  }
+}
+</style>
