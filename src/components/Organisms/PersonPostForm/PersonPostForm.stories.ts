9dc9772239a3317b31b9dff452e8765013e7adc3
bloom <non44921@gmail.com>
2024-05-01 17:11:29 +0900

    PersonPostForm story追加

---
+++ src/components/Organisms/PersonPostForm/PersonPostForm.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta } from "@storybook/vue3";
+import PersonPostForm from "./PersonPostForm.vue";
+
+const meta = {
+  title: "Organisms/PersonPostForm",
+  component: PersonPostForm,
+  tags: ["autodocs"],
+} satisfies Meta<typeof PersonPostForm>;
+
+export default meta;
+
+export const Default = {
+};
